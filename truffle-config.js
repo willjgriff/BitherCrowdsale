a7b0032aa50d032cc9908e843a71b718e666fd64
William Griffiths <willjgriff@gmail.com>
2018-12-05 16:22:51 +0000

    Added eth gas reported to see how it works. Also added deployment addresses to readme.

--- truffle-config.js
+++ truffle-config.js
@@ -95,7 +95,12 @@ module.exports = {
     // Set default mocha options here, use special reporters etc.
     mocha: {
         // timeout: 100000
-        enableTimeouts: false
+        enableTimeouts: false,
+        reporter: 'eth-gas-reporter',
+        reporterOptions : {
+            currency: 'GBP',
+            gasPrice: 20
+        }
     },
 
     // Configure your compilers
@@ -104,10 +109,10 @@ module.exports = {
             version: "0.4.25",    // Fetch exact version from solc-bin (default: truffle's version)
             // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
             // settings: {          // See the solidity docs for advice about optimization and evmVersion
-            //  optimizer: {
-            //    enabled: false,
-            //    runs: 200
-            //  },
+             optimizer: {
+               enabled: false,
+               runs: 200
+             },
             //  evmVersion: "byzantium"
             // }
         }
